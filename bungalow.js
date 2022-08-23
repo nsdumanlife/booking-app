ac651439d31b8f1dcb3eb0966aec39f4804e837c
NSDuman <dumanlife@yandex.com>
2022-08-23 16:24:23 +0300

    Add bungalow module

---
+++ bungalow.js
@@ -0,0 +1,18 @@
+class Bungalow {
+  constructor(location, capacity, price) {
+    this.location = location;
+    this.availability = true;
+    this.bookedDates = [];
+    this.city = "";
+    this.capacity = capacity;
+    this.price = price;
+    this.reviews = []; // user's reviews
+    this.rate = 0; // average stars are given by users
+    this.photos = []; //
+    this.services = []; // internet, barbecue,hot tub, pool, hot water, kitchen etc.
+    this.guests = [];
+    // id
+  }
+}
+
+module.exports = Bungalow;
